8449a062196fc3610653613e9204524f984dbf05
brunoeduardodev <brunoeduardo.dev@gmail.com>
2023-02-20 17:22:35 +0000

    feat: add card models

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -12,7 +12,7 @@ export const exampleRouter = createTRPCRouter({
     }),
 
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return [];
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
